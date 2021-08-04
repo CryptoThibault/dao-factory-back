eec9dff9207fa96a53fc0c0ec94abe4a2421a8ef
CryptoThibault <thibault.chalaoux@gmail.com>
2021-08-04 16:07:47 +0200

    base access control

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -13,14 +13,32 @@ contract Dao is AccessControl {
     GovernanceToken private _token;
     Treasury private _treasury;
 
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    bytes32 public constant EMPLOYEE_ROLE = keccak256("EMPLOYEE_ROLE");
+
     constructor(
         address initialOwner,
         uint256 initialSupply,
         string memory tokenName,
         string memory tokenSymbol
     ) {
+        _setRoleAdmin(ADMIN_ROLE, DEFAULT_ADMIN_ROLE);
+        _setupRole(ADMIN_ROLE, initialOwner);
         _governance = new Governance();
         _token = new GovernanceToken(initialOwner, initialSupply, tokenName, tokenSymbol);
         _treasury = new Treasury();
+    }
+
+    function governanceAddress() public view returns (address) {
+        return address(_governance);
+    }
+
+    function tokenAddress() public view returns (address) {
+        return address(_token);
+    }
+
+    function treasuryAddress() public view returns (address) {
+        return address(_treasury);
     }
 }
