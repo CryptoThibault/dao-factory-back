06861b4a8997a14663173bbd12783af433524a98
CryptoThibault <thibault.chalaoux@gmail.com>
2021-08-17 16:05:28 +0200

    new struct

---
+++ contractss/Dao.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.5;
+
+import "./Access.sol";
+import "./Governance.sol";
+
+contract Dao is Access {
+    Governance private _governance;
+
+    constructor(
+        address defaultAdmin,
+        string memory tokenName,
+        string memory tokenSymbol
+    ) {
+        _setupRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _governance = new Governance(address(this), tokenName, tokenSymbol);
+    }
+}
