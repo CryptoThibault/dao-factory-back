e9b01e95c019bfbfcf50cf53449e7d60a31b7a5c
CryptoThibault <thibault.chalaoux@gmail.com>
2021-09-08 16:40:04 +0200

    testing

--- test/Dao-test.js
+++ test/Dao-test.js
@@ -6,6 +6,7 @@ describe('Dao', async function () {
   const TOKEN_NAME = 'Business 1 Token';
   const TOKEN_SYMBOL = 'BS1';
   const DEFAULT_ADMIN_ROLE = ethers.utils.id('DEFAULT_ADMIN_ROLE');
+  const ADMIN_ROLE = ethers.utils.id('ADMIN_ROLE');
   beforeEach(async function () {
     [dev, alice] = await ethers.getSigners();
     Dao = await ethers.getContractFactory('Dao');
@@ -23,5 +24,9 @@ describe('Dao', async function () {
   });
   it('should asign alice as default admin', async function () {
     expect(await dao.hasRole(DEFAULT_ADMIN_ROLE, alice.address)).to.equal(true);
+  });
+  it('should asign dev as admin', async function () {
+    await dao.connect(alice).grantRole(ADMIN_ROLE, dev.address);
+    expect(await dao.hasRole(ADMIN_ROLE, dev.address)).to.equal(true);
   });
 });
